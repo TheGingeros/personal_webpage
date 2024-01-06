13ef96a88729b2e685887fbb3003839bfa9c163a
TheGingeros <frankie.glazier01@gmail.com>
2024-01-06 22:47:35 +0800

    add fadeout to nav menu

--- portfolio/nav.js
+++ portfolio/nav.js
@@ -28,15 +28,24 @@ document.addEventListener("DOMContentLoaded", function () {
         behavior: "smooth",
       });
     }
+    let timeout;
     // Toggle the navigation bar on burger icon click
     const burgerIcon = document.getElementById('burger-icon');
     burgerIcon.addEventListener('mouseenter', function () {
+        clearTimeout(timeout);
         navMenu.style.display = (navMenu.style.display === 'none' || navMenu.style.display === '') ? 'flex' : 'none';
         burgerIcon.style.display = (navMenu.style.display === 'none') ? 'block' : 'none';
     });
     // Close the navigation bar when the user moves the mouse out of it
     navMenu.addEventListener('mouseleave', function () {
-        navMenu.style.display = 'none';
-        burgerIcon.style.display = 'block';
+        clearTimeout(timeout);
+        timeout = setTimeout(() => {
+            navMenu.style.opacity = '0';
+            setTimeout(() => {
+                navMenu.style.display = 'none';
+                burgerIcon.style.display = 'block';
+                navMenu.style.opacity = '1'; // Reset opacity for future appearance
+            }, 500); // Match the transition duration
+        }, 125);
     });
 });
